a09b421c1991876e1f53b15b3393f6119cfe9661
Adam <thuc321@gmail.com>
2014-11-04 19:20:29 +0900

    update

--- Laplaces_Demon.r
+++ Laplaces_Demon.r
@@ -21,8 +21,71 @@ setwd("C:/Users/adam.nguyen/Documents/GitHub/LaplacesDemon")
 source(paste(c("C:/Users/adam.nguyen/Desktop/R/GH/", "library.R"), collapse = "")) #Get library
 source(paste(c("C:/Users/adam.nguyen/Desktop/R/GH/", "break_munging.txt"), collapse = "")) #Get tools
 
-#Install Laplace's Demon
-install.packages("devtools")
+#Install relevant libraries
+#install.packages("devtools")
+#install_github("Statisticat/LaplacesDemon")
 library(devtools)
-install_github("Statisticat/LaplacesDemon")
 library(LaplacesDemon)
+
+#Import data
+data(demonsnacks)
+
+#Convert dataframe into list
+N <- nrow(demonsnacks)
+J <- ncol(demonsnacks)
+y <- log(demonsnacks$Calories)
+X <- cbind(1, as.matrix(demonsnacks[,c(1,3:10)]))
+for (j in 2:J) {X[,j] <- CenterScale(X[,j])}
+mon.names <- c("LP","sigma")
+parm.names <- as.parm.names(list(beta=rep(0,J), log.sigma=0))
+MyData <- list(J=J, X=X, mon.names=mon.names, parm.names=parm.names, y=y)
+
+#Laplace's Demon estimation of Bayesian model of specified likelihood
+Model <- function(parm, Data)
+{
+### Parameters
+beta <- parm[1:Data$J]
+sigma <- exp(parm[Data$J+1])
+### Log(Prior Densities)
+beta.prior <- dnormv(beta, 0, 1000, log=TRUE)
+sigma.prior <- dhalfcauchy(sigma, 25, log=TRUE)
+### Log-Likelihood
+mu <- tcrossprod(beta, Data$X)
+LL <- sum(dnorm(Data$y, mu, sigma, log=TRUE))
+### Log-Posterior
+LP <- LL + sum(beta.prior) + sigma.prior
+Modelout <- list(LP=LP, Dev=-2*LL, Monitor=c(LP,sigma), yhat=mu,
+parm=parm)
+return(Modelout)
+}
+
+#Parameter Settings
+LP <- LL + sum(beta.prior) + sigma.prior
+Modelout <- list(LP=LP, Dev=-2*LL, Monitor=c(LP,sigma),
+yhat=mu, parm=parm)
+return(Modelout)
+
+#Initiate setting
+Initial.Values <- c(rep(0,J), log(1))
+set.seed(666)
+
+#Awaken the Demon
+Fit <- LaplacesDemon(Model, Data=MyData, Initial.Values,
+Covar=NULL, Iterations=2000, Status=100, Thinning=2,
+Algorithm="AMWG", Specs=list(Periodicity=10))
+
+#Demonic Suggestion
+str(Fit)
+Fit$Acceptance.Rate
+Fit
+Fit$Posterior2
+Consort(Fit)
+
+BurnIn <- Fit$Rec.BurnIn.Thinned
+plot(Fit, BurnIn, MyData, PDF=FALSE, Parms=Fit$Parameters)
+caterpillar.plot(Fit, Parms=1:10)
+
+#Visions of the Demon
+Pred <- predict(Fit, Model, MyData)
+summary(Pred, Discrep="Chi-Square")
+plot(Pred, Style="Density", Rows=1:9)
